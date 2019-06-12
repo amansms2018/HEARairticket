af5a7e569bfd6675d320ab069429186384591056
amanuel <azewgee@gmail.com>
2019-06-12 15:58:19 -0500

    Initial commit

---
+++ src/main/java/edu/mum/cs425/HEARairticket/HeaRairticketApplication.java
@@ -0,0 +1,13 @@
+package edu.mum.cs425.HEARairticket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HeaRairticketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HeaRairticketApplication.class, args);
+	}
+
+}
